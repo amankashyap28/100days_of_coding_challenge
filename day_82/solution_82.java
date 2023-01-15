7d1c085ab0633c50ec997dc8d97cf249b31fe3ca
amankashyap28 <114464469+amankashyap28@users.noreply.github.com>
2023-01-16 00:16:57 +0530

    Create solution_82.java

---
+++ day_82/solution_82.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    Solution ob = new Solution();
+		    long[] res = ob.nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+// User Function Template for JAVA
+
+class Solution{
+
+    public static long[] nextLargerElement(long[] arr, int n) {
+
+        // Your code here
+
+        Stack<Long>st=new Stack<>();
+
+        long res[]=new long[n];
+
+        st.push((long)-1);
+
+        // res[n-1]=-1;
+
+        for(int i=n-1;i>=0;i--)
+
+        {
+
+            while(st.size()!=1 && st.peek()<=arr[i])
+
+                st.pop();
+
+            //int nextgtr=(int)(st.isEmpty()?-1:st.peek());
+
+            
+
+            res[i]=st.peek();
+
+            st.push(arr[i]);
+
+        }
+
+        return res;
+
+    }
+}
+
+
